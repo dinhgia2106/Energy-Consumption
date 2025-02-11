7060e689589212edacff8fa14ec62b65c7da8a9f
dinhgia2106 <dinhgia2106@gmail.com>
2025-02-11 22:19:32 +0700

    Fix sthg

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from pandas.plotting import scatter_matrix
 
 # Tải dữ liệu
-df = pd.read_csv('energy-data-filtered')
+df = pd.read_csv('energy-data-filtered-full.csv')
 df['year'] = pd.to_datetime(df['year'], format='%Y')
 df.set_index('year', inplace=True)
 
